21d905bf75929f89409a7ede7b04faebb2998585
hyeonyway <kimhm1219@gmail.com>
2026-05-03 23:11:15 +0900

    fix: show email in user info response

--- userinfoserver/src/test/java/com/teamuta/userinfoserver/UserinfoserverApplicationTests.java
+++ userinfoserver/src/test/java/com/teamuta/userinfoserver/UserinfoserverApplicationTests.java
@@ -1,21 +1,10 @@
 package com.teamuta.userinfoserver;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import javax.sql.DataSource;
-
-@SpringBootTest(properties = {
-		"spring.autoconfigure.exclude=org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration,org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration"
-})
 class UserinfoserverApplicationTests {
-
-	@MockitoBean
-	private DataSource dataSource;
 
 	@Test
 	void contextLoads() {
 	}
-
 }
